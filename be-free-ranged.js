8fb8809c55fea1b6167f6d0f8ef565c8ab9f978f
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-22 06:38:04 -0500

    Initial commit

--- be-free-ranged.js
+++ be-free-ranged.js
@@ -3,19 +3,29 @@ export class BeFreeRanged extends EventTarget {
     async instantiate(pp, mold) {
         const { host, transformIslets, self, template } = pp;
         const { DTR } = await import('trans-render/lib/DTR.js');
+        const { getDestructArgs } = await import('trans-render/lib/getDestructArgs.js');
         const clone = template.content.cloneNode(true);
         for (const transformIslet of transformIslets) {
             const { transform, islet } = transformIslet;
+            let { isletDependencies } = transformIslet;
+            if (isletDependencies === undefined) {
+                isletDependencies = transformIslet.isletDependencies = getDestructArgs(islet);
+            }
             const ctx = {
                 host,
                 match: transform,
             };
             const transformer = new DTR(ctx);
+            let { transformDependencies } = transformIslet;
+            if (transformDependencies === undefined) {
+                transformDependencies = transformIslet.transformDependencies = await transformer.getDep();
+            }
             if (islet !== undefined) {
                 Object.assign(host, islet(host));
             }
             await transformer.transform(clone);
         }
+        console.log({ transformIslets });
         const cnt = clone.childNodes.length;
         if (self.nextElementSibling === null && self.parentElement !== null) {
             self.parentElement.appendChild(clone);
@@ -25,14 +35,13 @@ export class BeFreeRanged extends EventTarget {
             insertAdjacentClone(clone, self, 'afterend');
         }
         self.dataset.cnt = cnt + '';
-        const { getDestructArgs } = await import('trans-render/lib/getDestructArgs.js');
         const { getAdjacentChildren } = await import('trans-render/lib/getAdjacentChildren.js');
-        for (const transformIslet of transformIslets) {
-            const { transform, islet } = transformIslet;
-            //const isletSt = islet.toString();
-            const args = getDestructArgs(islet);
-            console.log({ args });
-        }
+        // for(const transformIslet of transformIslets!){
+        //     const {transform, islet} = transformIslet;
+        //     //const isletSt = islet.toString();
+        //     const args = getDestructArgs(islet);
+        //     console.log({args});
+        // }
         // host!.addEventListener('prop-changed', e => {
         //     const prop = (e as CustomEvent).detail.prop;
         //     if(observe!.includes(prop)){
